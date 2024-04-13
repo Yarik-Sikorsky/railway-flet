3925a67d19b02632ae2bdb98c9a256fca14888b0
Yarik-Sikorsky <105906426+Yarik-Sikorsky@users.noreply.github.com>
2024-04-13 22:43:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,27 @@ import os
 
 
 def main(page: ft.Page):
-    page.add(ft.Text("Hello, Railway!"))
+    page.title = "CupertinoNavigationBar Example"
+    page.navigation_bar = ft.CupertinoNavigationBar(
+        bgcolor=ft.colors.AMBER_100,
+        inactive_color=ft.colors.GREY,
+        active_color=ft.colors.BLACK,
+        on_change=lambda e: print("Selected tab:", e.control.selected_index),
+        destinations=[
+            ft.NavigationDestination(icon=ft.icons.EXPLORE, label="Explore"),
+            ft.NavigationDestination(icon=ft.icons.COMMUTE, label="Commute"),
+            ft.NavigationDestination(
+                icon=ft.icons.BOOKMARK_BORDER,
+                selected_icon=ft.icons.BOOKMARK,
+                label="Explore",
+            ),
+        ]
+    )
+    page.add(ft.SafeArea(ft.Text("Body!")))
+
+
 
 
 logging.basicConfig(level=logging.INFO)
 
-ft.app(target=main, view=None, port=int(os.getenv("PORT", 8502)))
+ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=int(os.getenv("PORT", 8502)))
